d0dde6d2709bb977e953b707e4431ab8a566751d
Salvador Cruz <salvador10@ucla.edu>
2026-08-28 12:28:48 -0700

    Add initial ESP32 soft-latch power control
    - Added temporary PWR_HOLD and PWR_BTN GPIO configuration
    - Verified AO3401 and 2N2222A power latch hardware
    - Confirmed battery-powered ESP32 remains on after startup button release
    - Prepared power system for shutdown logic implementation

--- src/main.cpp
+++ src/main.cpp
@@ -5,21 +5,31 @@
 //includes button and input control code
 #include "controls.h"
 
+//temporary test code to configure power latching system
+const int Power_HLD = 25;
+const int Power_BTN = 26; 
 
 void setup() {
+  //sets the power hold pin to output so its output could be used to activate and deactivate the NPN transistor
+  pinMode(Power_HLD, OUTPUT);
+  //immediately writes it high to active the transistor and keep the power supplied even after button is released
+  digitalWrite(Power_HLD, HIGH);
+
+  //sets the button pullup high so that we can detect a button press when it gets pulled low
+  pinMode(Power_BTN, INPUT_PULLUP);
   Serial.begin(115200);
 
-  if(!sd_card_setup())
-  {
-    return;
-  }
-  controlsSetup();
+  // if(!sd_card_setup())
+  // {
+  //   return;
+  // }
+  // controlsSetup();
 }
 
 void loop() {
-  if (encoderValueChanged())
-  {
-    Serial.println(getEncoderValue());
-  }
+  // if (encoderValueChanged())
+  // {
+  //   Serial.println(getEncoderValue());
+  // }
 }
 
